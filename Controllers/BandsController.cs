247b78819c583a757ffd06fbd51acb911d9c34fe
MainaGeorge <nuriszka@gmail.com>
2020-09-13 11:38:32 +0200

    Added get all bands and get specific band actions of bands contoller

---
+++ Controllers/BandsController.cs
@@ -0,0 +1,38 @@
+﻿using BandApi.Services.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BandApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BandsController : ControllerBase
+    {
+        private readonly IBandAlbumRepository _bandAlbumRepository;
+
+        public BandsController(IBandAlbumRepository bandAlbumRepository)
+        {
+            _bandAlbumRepository = bandAlbumRepository ?? throw new ArgumentNullException(nameof(bandAlbumRepository));
+        }
+
+        [HttpGet]
+        public IActionResult GetBands()
+        {
+            var bands = _bandAlbumRepository.GetBands();
+
+            return StatusCode(StatusCodes.Status200OK, bands);
+        }
+
+        [HttpGet("{bandId}")]
+        public IActionResult GetBand(Guid bandId)
+        {
+            var band = _bandAlbumRepository.GetBand(bandId);
+
+            if (band == null)
+                return StatusCode(StatusCodes.Status404NotFound);
+
+            return StatusCode(StatusCodes.Status200OK, band);
+        }
+    }
+}
