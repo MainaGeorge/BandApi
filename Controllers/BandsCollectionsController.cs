9d65bdaf72f3731c852348f08e00d5fc03154ac8
MainaGeorge <nuriszka@gmail.com>
2020-09-14 16:32:56 +0200

    Added adding multiple bands at once with their albums

---
+++ Controllers/BandsCollectionsController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using BandApi.DataTransferObjects;
+using BandApi.Entities;
+using BandApi.Services.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace BandApi.Controllers
+{
+    [Route("api/bandscollections")]
+    [ApiController]
+    public class BandsCollectionsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IBandAlbumRepository _bandAlbumRepository;
+
+        public BandsCollectionsController(IMapper mapper, IBandAlbumRepository bandAlbumRepository)
+        {
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _bandAlbumRepository = bandAlbumRepository ?? throw new ArgumentNullException(nameof(bandAlbumRepository));
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<Band>> CreateAGroupOfBandsAtAGo(IEnumerable<BandCreationDto> bandCreationDtos)
+        {
+            var bandsToCreate = _mapper.Map<IEnumerable<Band>>(bandCreationDtos);
+
+            foreach (var band in bandsToCreate)
+            {
+                _bandAlbumRepository.AddBand(band);
+            }
+
+            _bandAlbumRepository.Save();
+
+            var bandsToReturn = _mapper.Map<IEnumerable<BandDto>>(bandsToCreate);
+
+            return StatusCode(StatusCodes.Status200OK, bandsToReturn);
+        }
+    }
+}
